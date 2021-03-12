db5d73afa0890b5c5997f79bb2d53bf8e992dc5a
Adam <adam@quantsapp.com>
2021-03-12 16:24:20 +0530

    Custom Observable component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CustomObservableComponent } from './modules/custom-observable/custom-observable.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'custom-observable', component: CustomObservableComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
